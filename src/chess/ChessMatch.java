29a6ad5051bdce6a5dff162b71b2b45ad25adee2
julio <joufps@yahoo.com>
2022-06-12 21:57:57 -0300

    Primeira funcionalidade de peças ao tabuleiro

---
+++ src/chess/ChessMatch.java
@@ -0,0 +1,40 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
+
+//classe principal com as regras do Xadrez
+public class ChessMatch {
+    private Board board;
+    public ChessMatch(){
+        board = new Board(8,8); //tamanho do tabuleiro
+        initialSetup();
+
+    }
+
+    public ChessPiece[][] getPieces(){
+        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+
+        for(int i=0;i<board.getRows(); i++){
+            for(int j=0;j<board.getColumns(); j++){
+                mat[i][j] = (ChessPiece) board.piece(i,j); //downcast
+
+            }
+        }
+
+        return mat;
+
+    }
+
+    private void initialSetup(){
+        //iniciar a partida e colocar as peças no tabuleiro
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+        board.placePiece(new King(board, Color.BLACK), new Position(0,4));
+
+
+    }
+
+
+}
